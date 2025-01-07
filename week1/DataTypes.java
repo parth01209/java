18278602ca2461154ad160545fe38d68b6e28d2f
Parth <parthprabhune12@gmail.com>
2025-01-07 11:17:11 +0530

    Commit 1

---
+++ week1/DataTypes.java
@@ -0,0 +1,40 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int myInt = 5;
+        double myDouble = 5.5;
+        char myChar = 'A';
+        boolean myBoolean = true;
+
+        System.out.println(myInt + " " + myDouble + " " + myChar + " " + myBoolean);
+        // byte byt = 129; // Not possible in Java. Out of range
+        short sh = 898;
+        long longNum = 87878787l;
+        float floatNum = 5.5f;
+
+        System.out.println(sh + " " + longNum + " " + floatNum);
+
+    }
+
+    
+}
+
+// Data Types : 
+// Integer, Boolean, Float, Character
+// Integer - byte (1 byte), short (2 bytes), long (8 bytes), int (4 bytes)
+// Boolean - true, false
+// Float - float, double
+// Character - char
+// --------------------------------------------------
+// Ranges : 
+// int : -2^31 to 2^31 - 1
+// byte : -128 to 127
+// short : -2^15 to 2^15 - 1
+// long : -2^63 to 2^63 - 1
+// ========================
+// float : 1.4E-45 to 3.4E+38
+// double : 4.9E-324 to 1.8E+308
+// char : 0 to 65535
+// boolean : true, false
+
+
+
---
+++ week1/Var.java
@@ -0,0 +1,23 @@
+public class Var {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+
+        
+    }
+    
+}
+
+
+// 1.
+// Imagine your variable is a box. You can put anything inside the box. 
+// You can put a number, a string, a boolean,
+// an array, an object, etc. inside the variable.
+
+// 2. 
+// Java is stringly typed language 
+// It means that you can put any type of data inside a variable
+// but when you try to use that variable, you have to tell Java what type of data it
+// is. If you don't tell Java what type of data it is, it will throw an error.
